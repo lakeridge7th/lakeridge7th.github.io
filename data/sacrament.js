8a330a29c91e5e09f12f5788df7542cf93f4d552
lakeridge7th <80942416+lakeridge7th@users.noreply.github.com>
2021-06-13 08:37:40 -0600

    Updated for June 13th 2021

--- data/sacrament.js
+++ data/sacrament.js
@@ -1,20 +1,20 @@
 currentSacrament = {
-    'type': 'fast',
-    'date': 'June 6, 2021',
-    'presiding': 'Bishop Ryan Nonu',
+    'type': 'normal',
+    'date': 'June 13, 2021',
+    'presiding': 'Adam Wyatt',
     'conducting': 'Nick Arvanitas',
     'organist': 'Julianne Bassett',
     'chorister': 'Curtis Bassett',
     'music': null,
-    'hymns': [5, 174, 263],
-    'speakers': [],
-    'order': 'HHFH',
-    'invocation': 'Kendra Rangel',
-    'benediction': 'Spencer Rangel',
+    'hymns': ['TBA', 'TBA', 'TBA', 'TBA'],
+    'speakers': ['TBA', 'TBA', 'TBA'],
+    'order': 'HHSSHSH',
+    'invocation': 'TBA',
+    'benediction': 'TBA',
     'quote': '',
     'endAnnouncements': [
-        'We invite volunteers to help us sanitize the chapel after the meeting.'/*,
-        '(Second hour today will be Relief Society and Elder\'s Quorum)'//*/
+        'We invite volunteers to help us sanitize the chapel after the meeting.',
+        '(Second hour today will be Relief Society and Elder\'s Quorum)'
     ]
 };
 
